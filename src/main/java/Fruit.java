e6023c4e092b253efe000a97833b0ac2257d1e19
maxim1978 <rm83788@gmail.com>
2021-07-26 20:41:07 +0400

    Added classes:Fruit, Orange, Apple, Box.

---
+++ src/main/java/Fruit.java
@@ -0,0 +1,18 @@
+public class Fruit<T> {
+    private float weightFruit;
+
+
+    public Fruit(float weightFruit) {
+        this.weightFruit = weightFruit;
+        ;
+    }
+
+
+    public float getWeightFruit() {
+        return weightFruit;
+    }
+
+    public void setWeightFruit(float weightFruit) {
+        this.weightFruit = weightFruit;
+    }
+}
